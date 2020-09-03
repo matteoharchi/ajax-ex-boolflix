b6bb0dc6c38b1360ee275ff2cb61d73eab7a0a3e
Matteo Harchi <matteo.harchi@gmail.com>
2020-09-03 17:33:16 +0200

    Aggiunte serie TV

--- js/script.js
+++ js/script.js
@@ -6,12 +6,15 @@ $(document).ready(function(){
         $(".results").html("");
         movieSearch(ricerca);
 
+
     });
 
 
 
     //funzione ricerca film
     function movieSearch(data){
+        var source = $("#movie-template").html();
+        var template = Handlebars.compile(source);
         $.ajax(
             {
                 url: "https://api.themoviedb.org/3/search/movie?api_key=9fa935e79bf8d2bc13f91abd5721f117",
@@ -22,8 +25,6 @@ $(document).ready(function(){
                 },
                 success: function(movie){
                     var selectedmovie = movie.results;
-                    var source = $("#movie-template").html();
-                    var template = Handlebars.compile(source);
                     for (var i = 0; i < selectedmovie.length; i++) {
                         var item = {
                             "title": selectedmovie[i].title,
@@ -40,7 +41,33 @@ $(document).ready(function(){
                     alert("ERRORE")
                 }
         })
+        $.ajax(
+            {
+                url:"https://api.themoviedb.org/3/search/tv?api_key=9fa935e79bf8d2bc13f91abd5721f117",
+                method: "GET",
+                data: {
+                    query: data,
+                    language: "it-IT",
+                },
+                success: function(series){
+                    var selectedseries = series.results;
+                    for (var i = 0; i < selectedseries.length; i++) {
+                        var tv = {
+                            "title": selectedseries[i].name,
+                            "original-title": selectedseries[i].original_name,
+                            "language": language(selectedseries[i].original_language),
+                            "vote": insertStars(selectedseries[i].vote_average)
+                        };
+                        var html = template(tv);
+                        $(".results").append(html);
+
+                    }
+                }, error: function(){
+                    alert("ERRORE")
+                }
+            })
     }
+    //funzione stelle
     function insertStars(n){
         var stellePiene = "";
         var stelleVuote= "";
@@ -53,6 +80,7 @@ $(document).ready(function(){
         };
         return stellePiene+stelleVuote;
     };
+    // funzione lingua
     function language (lang){
         var en = '<img src="img/icons8-great-britain-22.png" alt="">'
         var it = '<img src="img/icons8-italy-22.png" alt="">'
