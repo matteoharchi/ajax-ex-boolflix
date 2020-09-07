019351f3328c41ba50775cc7520d0590c3ac78ec
Matteo Harchi <matteo.harchi@gmail.com>
2020-09-08 00:04:30 +0200

    funzione unica per profili trends e search + funzione bandiere sistemata

--- js/scriptprova.js
+++ js/scriptprova.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
     });
 
 
-    //funzione che inserisce i film e serie in trend
+    //funzione chiamata AJAX film e serie in trend
 
     function insertTrend(data, url, type){
         $.ajax(
@@ -45,34 +45,14 @@ $(document).ready(function(){
                     language: "it-IT"
                 },
                 success: function(movie){
-                    var selectedmovie = movie.results;
-                    for (var i = 0; i < selectedmovie.length; i++) {
-                        var poster = selectedmovie[i].poster_path;
-                        var src = 'https://image.tmdb.org/t/p/w342/' + poster;
-                        var item = {
-                            "poster":addPoster(poster, selectedmovie[i].title),
-                            "title": selectedmovie[i].title || selectedmovie[i].name,
-                            "original-title": selectedmovie[i].original_title || selectedmovie[i].original_name,
-                            "language": language(selectedmovie[i].original_language),
-                            "vote": insertStars(selectedmovie[i].vote_average),
-                            "type" : type,
-                            "storyline": storyLine(selectedmovie[i]),
-                        };
-                        if (movie.total_results > 0) {
-                            stampa(item, type);
-                        }else {
-                            noResult(type);
-                        }
-
-                    }
+                    movieProfile(movie, type)
                 }, error: function(){
                     alert("Si è presentato un errore nella ricerca di"+type)
                 }
         })
     }
-    //funzione ricerca film e serie
+    //funzione chiamata AJAX ricerca film e serie
     function search(data, url, type){
-
         $.ajax(
             {
                 url: url,
@@ -83,31 +63,37 @@ $(document).ready(function(){
                     language: "it-IT",
                 },
                 success: function(movie){
-                    var selectedmovie = movie.results;
-                    for (var i = 0; i < selectedmovie.length; i++) {
-                        var poster = selectedmovie[i].poster_path;
-                        var src = 'https://image.tmdb.org/t/p/w342/' + poster;
-                        var item = {
-                            "poster":addPoster(poster, selectedmovie[i].title),
-                            "title": selectedmovie[i].title || selectedmovie[i].name,
-                            "original-title": selectedmovie[i].original_title || selectedmovie[i].original_name,
-                            "language": language(selectedmovie[i].original_language),
-                            "vote": insertStars(selectedmovie[i].vote_average),
-                            "type" : type,
-                            "storyline": storyLine(selectedmovie[i]),
-                        };
-                        if (movie.total_results > 0) {
-                            stampa(item, type);
-                        }else {
-                            noResult(type);
-                        }
-
-                    }
+                    movieProfile(movie, type)
                 }, error: function(){
-                    alert("Si è presentato un errore nella ricerca di"+type)
+                    alert("Si è presentato un errore nella ricerca di "+type)
                 }
         })
     }
+
+// funzione raccolta dati card film/Serie
+    function movieProfile(movie, type){
+            var selectedmovie = movie.results;
+            for (var i = 0; i < selectedmovie.length; i++) {
+                var poster = selectedmovie[i].poster_path;
+                var src = 'https://image.tmdb.org/t/p/w342/' + poster;
+                var item = {
+                    "poster":addPoster(poster, selectedmovie[i].title),
+                    "title": selectedmovie[i].title || selectedmovie[i].name,
+                    "original-title": selectedmovie[i].original_title || selectedmovie[i].original_name,
+                    "language": language(selectedmovie[i].original_language),
+                    "vote": insertStars(selectedmovie[i].vote_average),
+                    "type" : type,
+                    "storyline": storyLine(selectedmovie[i]),
+                };
+                if (movie.total_results > 0) {
+                    stampa(item, type);
+                }else {
+                    noResult(type);
+                }
+
+            }
+        }
+    });
 
     //funzione stampa
 
@@ -117,7 +103,7 @@ $(document).ready(function(){
         var html = template(item);
         $(".results-" + type).append(html);
 
-    }
+    };
 
     //funzione stelle CORREGGI CON UN SOLO CICLO FOR
     function insertStars(n){
@@ -138,16 +124,14 @@ $(document).ready(function(){
     };
     // funzione lingua
     function language (lang){
-        var en = '<img src="img/icons8-great-britain-22.png" alt="">'
-        var it = '<img src="img/icons8-italy-22.png" alt="">'
-        if (lang == "en") {
-            lang = en;
-        } else if (lang == "it") {
-            lang = it;
+        var languages = ["it", "en"]
+        if (languages.includes(lang)) {
+            return '<img src="img/'+ lang+ '.png">'
+        }else {
+            return lang;
         }
-        return lang;
-    }
-});
+    };
+
 // funzione aggiunge poster, ricorda null senza ""!
 function addPoster(image, title){
     var src = 'https://image.tmdb.org/t/p/w342/' + image;
@@ -156,11 +140,11 @@ function addPoster(image, title){
     }else {
         return '<img src="img/no.jpg" class="dikembe poster">';
     }
-}
+};
 
 //funzione trama, cerco di fare in modo che cliccando i puntini di spospensione possa aprire tutta la trama.
 
 function storyLine(film){
     var trama = film.overview.substring(0,250);
     return trama
-}
+};
