95aad27c1a05a46c9cbdc9943f21ba39be381c76
Matteo Harchi <matteo.harchi@gmail.com>
2020-09-08 10:59:41 +0200

    messo hover info
    problema XML con funzione cast

--- js/scriptprova.js
+++ js/scriptprova.js
@@ -3,9 +3,12 @@ $(document).ready(function(){
     var urlTrendSerie = "https://api.themoviedb.org/3/trending/tv/week?";
     var urlFilm = "https://api.themoviedb.org/3/search/movie?";
     var urlTV = "https://api.themoviedb.org/3/search/tv?";
+    var urlCreditsFilm="https://api.themoviedb.org/3/movie/{movie_id}/credits?";
+    var urlCreditsTV="https://api.themoviedb.org/3/tv/{tv_id}/credits?";
 
-    insertTrend("movie", urlTrend, "Film");
-    insertTrend("tv", urlTrendSerie, "Serie");
+
+    insertTrend("movie", urlTrend, "Film", urlCreditsFilm);
+    insertTrend("tv", urlTrendSerie, "Serie", urlCreditsTV);
 
     $("#searchbutton").click(function(){
         $(".hide").removeClass("hide");
@@ -26,111 +29,131 @@ $(document).ready(function(){
             search(ricerca, urlTV, "Serie");
 
         }
+        });
+});
 
 
-    });
+
+// FUNZIONI
 
 
-    //funzione chiamata AJAX film e serie in trend
+//funzione chiamata AJAX film e serie in trend
 
-    function insertTrend(data, url, type){
-        $.ajax(
-            {
-                url: url,
-                method: "GET",
-                data: {
-                    api_key : "9fa935e79bf8d2bc13f91abd5721f117",
-                    media_type: data,
-                    time_window: "week",
-                    language: "it-IT"
-                },
-                success: function(movie){
-                    movieProfile(movie, type)
-                }, error: function(){
-                    alert("Si è presentato un errore nella ricerca di"+type)
-                }
-        })
-    }
-    //funzione chiamata AJAX ricerca film e serie
-    function search(data, url, type){
-        $.ajax(
-            {
-                url: url,
-                method: "GET",
-                data: {
-                    api_key : "9fa935e79bf8d2bc13f91abd5721f117",
-                    query: data,
-                    language: "it-IT",
-                },
-                success: function(movie){
-                    movieProfile(movie, type)
-                }, error: function(){
-                    alert("Si è presentato un errore nella ricerca di "+type)
-                }
-        })
-    }
-
-// funzione raccolta dati card film/Serie
-    function movieProfile(movie, type){
-            var selectedmovie = movie.results;
-            for (var i = 0; i < selectedmovie.length; i++) {
-                var poster = selectedmovie[i].poster_path;
-                var src = 'https://image.tmdb.org/t/p/w342/' + poster;
-                var item = {
-                    "poster":addPoster(poster, selectedmovie[i].title),
-                    "title": selectedmovie[i].title || selectedmovie[i].name,
-                    "original-title": selectedmovie[i].original_title || selectedmovie[i].original_name,
-                    "language": language(selectedmovie[i].original_language),
-                    "vote": insertStars(selectedmovie[i].vote_average),
-                    "type" : type,
-                    "storyline": storyLine(selectedmovie[i]),
-                };
-                if (movie.total_results > 0) {
-                    stampa(item, type);
-                }else {
-                    noResult(type);
-                }
-
+function insertTrend(data, url, type){
+    $.ajax(
+        {
+            url: url,
+            method: "GET",
+            data: {
+                api_key : "9fa935e79bf8d2bc13f91abd5721f117",
+                media_type: data,
+                time_window: "week",
+                language: "it-IT"
+            },
+            success: function(movie){
+                movieProfile(movie, type)
+            }, error: function(){
+                alert("Si è presentato un errore nella ricerca di"+type)
             }
-        }
-    });
-
-    //funzione stampa
-
-    function stampa(item, type){
-        var source = $("#template").html();
-        var template = Handlebars.compile(source);
-        var html = template(item);
-        $(".results-" + type).append(html);
-
-    };
-
-    //funzione stelle CORREGGI CON UN SOLO CICLO FOR
-    function insertStars(n){
-        var resto = n%2;
-        var grade = Math.floor(n/2);
-        var stars = "";
-        for (var i = 0; i < 5; i++) {
-            if (i<grade) {
-                stars += '<i class="fas fa-star full"></i>';
-            }else if (resto != 0) {
-                stars += '<i class="fas fa-star-half-alt full"</i>';
-                resto = 0;
-            } else {
-                stars += '<i class="fas fa-star empty"></i>'
+    })
+}
+//funzione chiamata AJAX ricerca film e serie
+function search(data, url, type){
+    $.ajax(
+        {
+            url: url,
+            method: "GET",
+            data: {
+                api_key : "9fa935e79bf8d2bc13f91abd5721f117",
+                query: data,
+                language: "it-IT",
+            },
+            success: function(movie){
+                movieProfile(movie, type)
+            }, error: function(){
+                alert("Si è presentato un errore nella ricerca di "+type)
             }
+    })
+}
+
+// costruzione profili card film/Serie
+function movieProfile(movie, type, urlCast){
+        var selectedmovie = movie.results;
+        for (var i = 0; i < selectedmovie.length; i++) {
+            var poster = selectedmovie[i].poster_path;
+            var src = 'https://image.tmdb.org/t/p/w342/' + poster;
+            var item = {
+                "poster":addPoster(poster, selectedmovie[i].title),
+                "title": selectedmovie[i].title || selectedmovie[i].name,
+                "original-title": selectedmovie[i].original_title || selectedmovie[i].original_name,
+                "language": language(selectedmovie[i].original_language),
+                "vote": insertStars(selectedmovie[i].vote_average),
+                "type" : type,
+                "storyline": storyLine(selectedmovie[i]),
+                // "actors": cast(urlCast, selectedmovie[i]),
+            };
+            if (movie.total_results > 0) {
+                stampa(item, type);
+            }else {
+                noResult(type); //ancora da fare
+            }
+
         }
-        return stars
-    };
-    // funzione lingua
-    function language (lang){
-        var languages = ["it", "en"]
-        if (languages.includes(lang)) {
-            return '<img src="img/'+ lang+ '.png">'
-        }else {
-            return lang;
+    }
+
+//funzione per Cast
+
+// function cast(url, movie){
+//     $.ajax (
+//         {
+//             url: url,
+//             api_key : "9fa935e79bf8d2bc13f91abd5721f117",
+//             data:{
+//                 movie_id: movie.id,
+//             },
+//             success:function(response){
+//                 var actors = response.cast.slice(0,4);
+//                 return actors
+//             }
+//     })
+//
+// }
+//funzione stampa
+
+function stampa(item, type){
+    var source = $("#template").html();
+    var template = Handlebars.compile(source);
+    var html = template(item);
+    $(".results-" + type).append(html);
+
+};
+
+//funzione stelle valutazione
+function insertStars(n){
+    var resto = n%2;
+    var grade = Math.floor(n/2);
+    var stars = "";
+    for (var i = 0; i < 5; i++) {
+        if (i<grade) {
+            stars += '<i class="fas fa-star full"></i>';
+        }else if (resto != 0) {
+            stars += '<i class="fas fa-star-half-alt full"</i>';
+            resto = 0;
+        } else {
+            stars += '<i class="fas fa-star empty"></i>'
         }
-    };
+    }
+    return stars
+};
+// funzione lingua
+function language (lang){
+    var languages = ["it", "en"]
+    if (languages.includes(lang)) {
+        return '<img src="img/'+ lang+ '.png">'
+    }else {
+        return lang;
+    }
+};
 
 // funzione aggiunge poster, ricorda null senza ""!
 function addPoster(image, title){
@@ -145,6 +168,6 @@ function addPoster(image, title){
 //funzione trama, cerco di fare in modo che cliccando i puntini di spospensione possa aprire tutta la trama.
 
 function storyLine(film){
-    var trama = film.overview.substring(0,250);
+    var trama = film.overview;
     return trama
 };
